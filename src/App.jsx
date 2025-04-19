ea37917af4acb9838d813acf33f5aad084d8473b
Xonzodasss <xonzodaxonasrorxonova@gmail.com>
2025-04-19 11:01:56 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+      <h1>Asrorxonova Xonzodaxon</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi natus, quam est veritatis molestiae earum quaerat aliquid totam nulla animi praesentium, beatae repellat, tenetur non officia esse voluptatibus consequuntur autem?</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Tailwind css</li>
+        <li>Javascript</li>
+        <li>React</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
